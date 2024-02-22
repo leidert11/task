9f4cfae44ae38672f69cccbd2bb710e6833f204b
leidert11 <ltami216@gmail.com>
2024-02-21 23:15:55 -0500

    refactor: :recycle: refactorizacion del  index principal e implementar el patrón singleton en index de modelos

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,48 +3,69 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+let sequelizeInstance = null;
+
+class Database {
+  constructor() {
+    if (sequelizeInstance) {
+      return sequelizeInstance;
+    }
+    sequelizeInstance = this.initSequelize();
+    this.sequelize = sequelizeInstance;
+    this.models = this.initModels();
+  }
+
+  initSequelize() {
+    if (config.use_env_variable) {
+      return new Sequelize(process.env[config.use_env_variable], config);
+    } else {
+      return new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config
+      );
+    }
+  }
+
+  initModels() {
+    let models = {};
+    fs.readdirSync(__dirname)
+      .filter((file) => {
+        return (
+          file.indexOf(".") !== 0 &&
+          file !== basename &&
+          file.slice(-3) === ".js" &&
+          file.indexOf(".test.js") === -1
+        );
+      })
+      .forEach((file) => {
+        const model = require(path.join(__dirname, file))(
+          sequelizeInstance,
+          Sequelize.DataTypes
+        );
+        models[model.name] = model;
+      });
+
+    Object.keys(models).forEach((modelName) => {
+      if (models[modelName].associate) {
+        models[modelName].associate(models);
+      }
+    });
+
+    return models;
+  }
 }
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".js" &&
-      file.indexOf(".test.js") === -1
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
+const database = new Database();
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
+db.sequelize = database.sequelize;
 db.Sequelize = Sequelize;
+Object.assign(db, database.models);
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const router = require("./backend/routers");
 const path = require("path");
-const bodyParser = require("body-parser");
+const compression = require("compression");
 
 const app = express();
-app.use(morgan("dev"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(compression());
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", router);
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("¡Ups! Algo salió mal.");
+});
 
 app.set("port", process.env.PORT || 4000);
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(app.get("port"), () => {
-    console.log("Server on port " + app.get("port") + " on dev");
+    console.log(`Server listening on port ${app.get("port")}`);
   });
 }
 
